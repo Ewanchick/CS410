d1247cfaf06841e7ae4f0e19b719969124eea850
Ewanchick <ewanchic@wvup.edu>
2026-03-05 10:32:52 -0500

    finished refactoring CommandHandler

--- ZuulRemake/ZuulRemake/Classes/Player.cs
+++ ZuulRemake/ZuulRemake/Classes/Player.cs
@@ -15,7 +15,7 @@ namespace ZuulRemake.Classes
      */
     public class Player : Entity
     {
-        private readonly List<Item> Inventory = new();
+        protected List<Item> Inventory = new();
         private readonly Stack<Room> PreviousRooms = new();
         private readonly NavigationManager navigationManager = new();
 
@@ -45,7 +45,7 @@ namespace ZuulRemake.Classes
             }
         }
 
-        /* ------------------------------ LEVEL ------------------------------ */
+        /* ------------------------------ LEVEL HP ------------------------------ */
 
         /**
         * Increases Player Level (damage dealt)
@@ -53,6 +53,10 @@ namespace ZuulRemake.Classes
         public void LevelUp(int lvl)
         {
             Level += lvl;
+        }
+        public void AddHp(int hp)
+        {
+            HP += hp;
         }
 
         /* ------------------------------ WEIGHT & INVENTORY ------------------------------ */
@@ -89,6 +93,17 @@ namespace ZuulRemake.Classes
         {
             return Inventory.ToString();
         }
+        public Item? GetItem(string name)
+        {
+            if (string.IsNullOrWhiteSpace(name)) return null;
+            return Inventory.FirstOrDefault(i => i.Name != null && Equals(name, StringComparison.OrdinalIgnoreCase));
+        }
+
+        public List<Item> GetItems()
+        {
+            return Inventory;
+        }
+
 
         public Item? GetItem(string name)
         {
