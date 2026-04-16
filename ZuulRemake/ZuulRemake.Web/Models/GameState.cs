f9d55cd256d214a333c2597c46edb6c890d461fd
Ethan R. <eriddle1@wvup.edu>
2026-04-16 00:24:23 -0400

    kms

---
+++ ZuulRemake/ZuulRemake.Web/Models/GameState.cs
@@ -0,0 +1,12 @@
+﻿namespace ZuulRemake.Web.Models
+{
+    public class GameState
+    {
+        public string RoomName { get; set; } = "";
+        public string RoomDescription { get; set; } = "";
+        public int PlayerHP { get; set; }
+
+        public List<string> Messages { get; set; } = new();
+        public List<string> Inventory { get; set; } = new();
+    }
+}
--- ZuulRemake/ZuulRemake.Web/Pages/PlayGame.cshtml.cs
+++ ZuulRemake/ZuulRemake.Web/Pages/PlayGame.cshtml.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using ZuulRemake.Classes;
+using ZuulRemake.Web.Models;
 
 namespace ZuulRemake.Web.Pages
 {
     public class PlayGameModel : PageModel
     {
         private readonly ILogger<PlayGameModel> _logger;
+        public GameState GameState { get; set; } = new();
 
         public PlayGameModel(ILogger<PlayGameModel> logger)
         {
@@ -14,7 +17,32 @@ namespace ZuulRemake.Web.Pages
 
         public void OnGet()
         {
+            var game = new Game(new Player("Test Player"));
+            var player = game.Player;
 
+            string roomName = "ERROR! ";
+            string roomDesc = "Room not found. ";
+
+            try
+            {
+                var room = player.GetCurrentRoom();
+
+                roomName = room.Name;
+                roomDesc = room.GetLongDescription();
+            }
+            catch (Exception ex)
+            {
+                roomDesc = $"Error: {ex.Message}";
+            }
+
+            GameState = new GameState
+            {
+                RoomName = roomName,
+                RoomDescription = roomDesc,
+                PlayerHP = player.HP,
+                Inventory = player.GetItems().Select(i => i.Name).ToList(),
+                Messages = new List<string> { "Game loaded." }
+            };
         }
     }
 }
